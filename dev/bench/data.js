54d8ba91c8e56a4b9b46733ec7737623e8de7b5b
github-action-benchmark <github@users.noreply.github.com>
2020-04-18 06:39:06 +0000

    add Rust Benchmark (cargo) benchmark result for 16da483f486f1d7f9eb1832e3aecf5b908dd7695

---
+++ dev/bench/data.js
@@ -0,0 +1,134 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1587191946767,
+  "repoUrl": "https://github.com/schungx/rhai",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Stephen.Chung@intexact.com",
+            "name": "Stephen Chung",
+            "username": "schungx"
+          },
+          "committer": {
+            "email": "Stephen.Chung@intexact.com",
+            "name": "Stephen Chung",
+            "username": "schungx"
+          },
+          "distinct": true,
+          "id": "16da483f486f1d7f9eb1832e3aecf5b908dd7695",
+          "message": "Set theme jekyll-theme-slate",
+          "timestamp": "2020-04-18T14:30:16+08:00",
+          "tree_id": "68dcf04c7364f9f394f843a3723d9de40b924462",
+          "url": "https://github.com/schungx/rhai/commit/16da483f486f1d7f9eb1832e3aecf5b908dd7695"
+        },
+        "date": 1587191942942,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_eval_expression_number_literal",
+            "value": 369,
+            "range": "± 77",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_eval_expression_number_operators",
+            "value": 702,
+            "range": "± 103",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_eval_expression_single",
+            "value": 45,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_eval_scope_complex",
+            "value": 866,
+            "range": "± 34",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_eval_scope_longer",
+            "value": 1130,
+            "range": "± 269",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_eval_scope_multiple",
+            "value": 423,
+            "range": "± 200",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_eval_scope_single",
+            "value": 422,
+            "range": "± 60",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_type_field",
+            "value": 369,
+            "range": "± 10",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_type_method",
+            "value": 314,
+            "range": "± 17",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_type_method_nested",
+            "value": 679,
+            "range": "± 106",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_type_method_with_params",
+            "value": 393,
+            "range": "± 9",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_iterations_1000",
+            "value": 860104,
+            "range": "± 54662",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_iterations_fibonacci",
+            "value": 28101071,
+            "range": "± 2058183",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_parse_full",
+            "value": 9895,
+            "range": "± 803",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_parse_primes",
+            "value": 28698,
+            "range": "± 2851",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_parse_simple",
+            "value": 2274,
+            "range": "± 142",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_parse_single",
+            "value": 315,
+            "range": "± 48",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
