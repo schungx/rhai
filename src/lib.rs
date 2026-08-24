a8aade2a37bda0b370318fcce4bb784cad0def08
Stephen Chung <schungx@live.com>
2026-08-24 08:32:26 +0800

    Remove limitation on no-std with wasm.

--- src/lib.rs
+++ src/lib.rs
@@ -468,11 +468,6 @@ compile_error!("`only_i32` and `only_i64` cannot be used together");
 #[cfg(feature = "wasm-bindgen")]
 compile_error!("`wasm-bindgen` cannot be used with `no-std`");
 
-#[cfg(target_family = "wasm")]
-#[cfg(not(target_os = "unknown"))]
-#[cfg(feature = "no_std")]
-compile_error!("`no_std` cannot be used for WASM target that is not `wasm32-unknown-unknown`");
-
 #[cfg(not(target_family = "wasm"))]
 #[cfg(feature = "wasm-bindgen")]
 compile_error!("`wasm-bindgen` cannot be used for non-WASM target");
